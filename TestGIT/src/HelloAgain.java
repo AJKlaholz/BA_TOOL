0d7f6d8e9c8d9dcb3ba0e8eca288b657db477c9f
PC_AJKlaholz <adrian.klaholz@gmail.com>
2016-10-31 21:58:48 +0100

    tt

--- TestGIT/src/HelloAgain.java
+++ TestGIT/src/HelloAgain.java
@@ -4,6 +4,8 @@ public class HelloAgain {
 	public static void main(String[] args) {
 		System.out.println("Hello Again");
 		System.out.println("Hello Again");
+		
+		System.out.println("Und du geht das auch so?");
 
 	}
 
