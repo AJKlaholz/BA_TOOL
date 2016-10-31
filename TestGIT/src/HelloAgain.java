b13a5fe87281e358a78746cda64f66a7efd12ade
AJKlaholz <adrian.klaholz@gmail.com>
2016-10-31 21:57:17 +0100

    Update HelloAgain.java

--- TestGIT/src/HelloAgain.java
+++ TestGIT/src/HelloAgain.java
@@ -4,9 +4,6 @@ public class HelloAgain {
 	public static void main(String[] args) {
 		System.out.println("Hello Again");
 		System.out.println("Hello Again");
-		System.out.println("Hello Again");
-
-		System.out.println("Hello Again");
 
 	}
 
