db55c9fe5dcb13298a582138e5cf2226ae163448
AJKlaholz <adrian.klaholz@gmail.com>
2016-10-31 16:33:32 +0100

    add syso

--- TestGIT/src/Testit.java
+++ TestGIT/src/Testit.java
@@ -3,7 +3,7 @@ public class Testit {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Worlddd");// TODO Auto-generated method stub
-
+		System.out.println("Guten Tag");
 	}
 
 }
