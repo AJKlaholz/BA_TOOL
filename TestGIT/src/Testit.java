836814a575d148ff238cf035f24a0698f222484a
PC_AJKlaholz <adrian.klaholz@gmail.com>
2016-10-31 21:38:09 +0100

    hat es auch hier geklappt ?

--- TestGIT/src/Testit.java
+++
@@ -1,9 +0,0 @@
-
-public class Testit {
-
-	public static void main(String[] args) {
-		System.out.println("Hello Worlddd");// TODO Auto-generated method stub
-		System.out.println("Guten Tag");
-	}
-
-}
