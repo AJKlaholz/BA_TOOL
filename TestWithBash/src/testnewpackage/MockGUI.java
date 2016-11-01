396c8908382c1a0c3eb12f8f9cc781de2f435b6d
PC_AJKlaholz <adrian.klaholz@gmail.com>
2016-11-01 11:12:11 +0100

    added save com

--- TestWithBash/src/testnewpackage/MockGUI.java
+++ TestWithBash/src/testnewpackage/MockGUI.java
@@ -1,11 +1,13 @@
 package testnewpackage;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-public class MockGUI extends JFrame {
+public class MockGUI extends JFrame implements ActionListener {
 	private JLabel text = new JLabel();
 	private JButton knopf = new JButton();
 	
@@ -17,6 +19,7 @@ public class MockGUI extends JFrame {
 		this.setLayout(new FlowLayout());
 		
 		knopf.setText("Save");
+		knopf.addActionListener(this);
 		this.add(knopf);
 		this.pack();
 		this.setVisible(true);
@@ -25,6 +28,18 @@ public class MockGUI extends JFrame {
 
 	public static void main(String[] args) {
 		new MockGUI();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		JFrame f = new JFrame();
+		f.setTitle("Message");
+		f.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		JLabel jl = new JLabel("Object was saved");
+		f.add(jl);
+		f.pack();
+		f.setVisible(true);
+		
 	}
 
 }
