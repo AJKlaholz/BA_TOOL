1a29306a62a262818a159a7807546206d6cf3615
PC_AJKlaholz <adrian.klaholz@gmail.com>
2016-11-01 10:37:15 +0100

    added button "save"

--- TestWithBash/src/testnewpackage/MockGUI.java
+++ TestWithBash/src/testnewpackage/MockGUI.java
@@ -1,9 +1,30 @@
 package testnewpackage;
+import java.awt.FlowLayout;
 
-public class MockGUI {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class MockGUI extends JFrame {
+	private JLabel text = new JLabel();
+	private JButton knopf = new JButton();
+	
+	public MockGUI(){
+		this.setTitle("Mockup GUI");
+		
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		this.setLayout(new FlowLayout());
+		
+		knopf.setText("Save");
+		this.add(knopf);
+		this.pack();
+		this.setVisible(true);
+		
+	}
 
 	public static void main(String[] args) {
-		
+		new MockGUI();
 	}
 
 }
